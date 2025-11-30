3ef75fcb241dd55770c1b115fd7dc956134d635f
Ville Heikkilä <mail@villeheikkila.com>
2025-11-30 11:18:58 +0200

    Add initial ios client

---
+++ koditon/Source/koditonApp.swift
@@ -0,0 +1,17 @@
+//
+//  koditonApp.swift
+//  koditon
+//
+//  Created by Ville Heikkilä on 30.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct koditonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
